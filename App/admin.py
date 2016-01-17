981161b7bdb31675d8332b25b678f7b306b0648e
DenerRodrigues <dl.rodrigues94@gmail.com>
2016-01-17 16:37:15 -0200

    CRUD - Servidor

--- App/admin.py
+++ App/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from models import Servidor, Provedor
+
 
 # Register your models here.
+
+class AdminServidor(admin.ModelAdmin):
+    list_display = ["descricao", "cpu", "ram", "disco"]
+    list_editable = ["descricao", "cpu", "ram", "disco"]
+    list_filter = ["descricao", "cpu", "ram", "disco"]
+    search_fields = ["descricao", "cpu", "ram", "disco"]
+
+
+class AdminProvedor(admin.ModelAdmin):
+    list_display = ["descricao", "logomarca"]
+    list_editable = ["descricao"]
+    list_filter = ["descricao"]
+    search_fields = ["descricao"]
+
+
+admin.site.register(Servidor, AdminServidor)
+admin.site.register(Provedor, AdminProvedor)
--- App/models.py
+++ App/models.py
@@ -2,4 +2,31 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
 # Create your models here.
+
+class Provedor(models.Model):
+    descricao = models.CharField(max_length=120, verbose_name="Descricao", null=False)
+    logomarca = models.ImageField(verbose_name="Logomarca", null=True, blank=True)
+
+    def __unicode__(self):
+        return self.descricao
+
+    class Meta:
+        verbose_name_plural = "Provedores"
+
+
+class Servidor(models.Model):
+    descricao = models.CharField(max_length=120, verbose_name="Descricao", null=False)
+    cpu = models.IntegerField(verbose_name="Cpu")
+    ram = models.CharField(max_length=10, verbose_name="Ram")
+    disco = models.CharField(max_length=10, verbose_name="Disco")
+    so = models.CharField(max_length=60, verbose_name="Sistema Operacional")
+    preco = models.FloatField(verbose_name="Preco")
+    provedor = models.ForeignKey(Provedor, verbose_name="Provedor")
+
+    def __unicode__(self):
+        return self.descricao
+
+    class Meta:
+        verbose_name_plural = "Servidores"
